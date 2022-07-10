3cc7ccf467e4aefeec6d4d24c356f6e817b9ccbc
mczionjohnson <87995811+mczionjohnson@users.noreply.github.com>
2022-07-10 21:41:17 +0100

    after merge

--- config/index.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config()
-
-module.exports.SECRET = process.env.APP_SECRET
-// get this during development from the .env folder
--- config/indexAdmin.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config()
-
-module.exports.SECRETADMIN = process.env.APP_SECRETADMIN
-// get this during development from the .env folder
